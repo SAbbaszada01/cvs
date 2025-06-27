3d079690d829375a89082019db80c75163e26cf1
SAbbaszada01 <sultanabbas371@gmail.com>
2025-06-27 11:01:54 +0400

    Create script.js

---
+++ static/script.js
@@ -0,0 +1,12 @@
+async function generateCV() {
+    const prompt = document.getElementById('cvPrompt').value;
+    const response = await fetch('/create_cv', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ data: prompt }),
+    });
+    const result = await response.json();
+    document.getElementById('cvResult').innerText = result.cv;
+}
